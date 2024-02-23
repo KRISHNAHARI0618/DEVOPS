1b38c2a62d3e4b08e1837d5e2c8dd606c200d696
HariBalaji4674 <hari.balaji4674@gmail.com>
2024-02-23 09:28:32 +0530

    23-02-2024

---
+++ Python/Big-O-Notation.py
@@ -0,0 +1,10 @@
+'''
+Big O Notation: Metric Used to describe the efficiency of algorithms
+
+'''
+
+04:00 am to 07:00 am --> Kubernetes
+08:00 am to 10:00 am --> Docker
+10:00 am to 12:00 am --> Kubernetes
+12:00 pm to 01:00 pm --> Shell-Script
+
---
+++ Python/Lambda-01.py
@@ -0,0 +1,7 @@
+name = lambda a,b : a+b
+print(name(10,20))
+
+x = [2,4,5,6,7,9,24]
+for i in x:
+  name1 = lambda i : i%2 == 0
+  print(name1(i))
