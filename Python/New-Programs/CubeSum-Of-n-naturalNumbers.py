6e36aa4c1b9b305d19c0ec0e8aa71a0527e8196d
HariBalaji4674 <hari.balaji4674@gmail.com>
2024-03-07 16:59:14 +0530

    07-03-2024

--- Python/New-Programs/CubeSum-Of-n-naturalNumbers.py
+++ Python/New-Programs/CubeSum-Of-n-naturalNumbers.py
@@ -10,3 +10,12 @@ for i in range(0,number+1):
   sum = sum + i*i*i
   print("Sum Of Numbers :", sum)
 
+# (n*(n+1)/2)
+
+def cubes_number(n):
+  x = (n*(n+1)/2) # 7 * 8/2 = 32
+  return (int)(x*x) # 32*32
+
+print(cubes_number(7))
+
+
