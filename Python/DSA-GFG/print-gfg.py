f0846e687684f8582796a04d59ec72897dec06e0
HariBalaji4674 <hari.balaji4674@gmail.com>
2024-03-14 09:39:37 +0530

    14-03-2024

--- Python/DSA-GFG/print-gfg.py
+++ Python/DSA-GFG/print-gfg.py
@@ -1,3 +1,27 @@
 print(" Om Namo Vinayakaya Namahh  ")
 print(" Om Namo Venkateshaya Namahh")
 
+# Mathematical Problems:
+#1: Sum of First N Natural Numers : n*(n+1)/2
+
+#2: Counter
+
+def counter(n):
+  res = 0
+  while n != 0:
+    n = n//10
+    res = res + 1
+  return res
+print(counter(1234))
+
+# Fibonacci Series:
+# Palindrome:
+def counter(n):
+  res = 0
+  temp = n
+  while n != 0:
+    # n = n%10
+    res = res + 10*(n%10)
+    n = n//10
+  return res == temp
+print(counter(121))
