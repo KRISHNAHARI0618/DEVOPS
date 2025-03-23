6ced7540ae45cacb18e4cf71f4f2fd1e78476671
Hari Vardhan Reddy Peddireddy <Harivardhanreddy.Peddireddy@blueyonder.com>
2025-03-23 11:49:15 +0530

    Added GCP-Terraform

---
+++ GCP-Terraform/01-GCP/main.tf
@@ -0,0 +1,10 @@
+resource local_file peddireddy {
+    filename = "sample.txt"
+    content = "I Love Terraform Docker Kubernetes Argo CD"
+  
+}
+resource local_file peddireddy2 {
+    filename = "sample2.txt"
+    content = "I Love Terraform Docker Kubernetes Argo CD"
+  
+}
